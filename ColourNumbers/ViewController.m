fab96a38f946d8ddffc465662f2dba1fce4a7aa8
Vitaliy Kostromin <HellVitalii@gmail.com>
2019-07-15 13:16:03 +0700

    Stable simple version with editing numbers

---
+++ ColourNumbers/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  ColourNumbers
+//
+//  Created by stager on 12/07/2019.
+//  Copyright © 2019 sbs. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
